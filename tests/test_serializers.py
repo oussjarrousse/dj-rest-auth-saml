1530236c29a9dc52c733a7474ab7012bc6412448
Oussama Jarrousse <oussama@jarrousse.org>
2024-01-11 19:27:20 +0100

    adding serializers tests

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -5,6 +5,7 @@ from dj_rest_auth.registration.serializers import SocialLoginSerializer
 from django.apps import apps
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
+from rest_framework.exceptions import ValidationError
 from rest_framework.test import APIRequestFactory
 
 from dj_rest_auth_saml.serializers import SAMLSocialLoginSerializer
@@ -14,15 +15,37 @@ from dj_rest_auth_saml.views import CustomFinishACSView
 
 
 class Tests_SAMLSocialLoginSerializer:
-    pass
-    # @pytest.mark.django_db
-    # def test_validate(self, unauthenticated_api_client):
-    #     client = unauthenticated_api_client
-    #     settings.APP_HOST = "example.com"
-    #     add_default_saml_application(apps, None)
+    def test_post_signup(self):
+        # client = unauthenticated_api_client
+        # settings.APP_HOST = "example.com"
+        # add_default_saml_application(apps, None)
+        serializer = SAMLSocialLoginSerializer()
+        serializer.post_signup(None, None)
 
-    #     factory = APIRequestFactory()
-    #     request = factory.get("/api/v1/core/info/")
+    @pytest.mark.django_db
+    def test_validate(self, unauthenticated_api_client):
+        client = unauthenticated_api_client
+        settings.APP_HOST = "example.com"
+        add_default_saml_application(apps, None)
+
+        serializer = SAMLSocialLoginSerializer()
+        assert serializer
+        assert isinstance(serializer, SAMLSocialLoginSerializer)
+        assert isinstance(serializer, SocialLoginSerializer)
+
+        with pytest.raises(AttributeError):
+            serializer.validate(None)
+
+        factory = APIRequestFactory()
+        request = factory.post("/")
+
+        context = {"request": request, "view": None}
+        serializer = SAMLSocialLoginSerializer(context=context)
+        with pytest.raises(ValidationError):
+            serializer.validate(None)
+
+        view = CustomFinishACSView()
+
     #     request.user = AnonymousUser()
 
     #     setattr(request, "session", client.session)
@@ -48,11 +71,8 @@ class Tests_SAMLSocialLoginSerializer:
 
     #     setattr(request, "saml_acs_session", store)
 
-    #     view = CustomFinishACSView()
     #     view.organization_slug = "example"
     #     serializer = SAMLSocialLoginSerializer(context={"request": request, "view": view})
-    #     assert isinstance(serializer, SAMLSocialLoginSerializer)
-    #     assert isinstance(serializer, SocialLoginSerializer)
 
     #     attrs = {
     #     }
